6862fb2131183b256a5cbe1a2532756e06e82995
mrjeoffrey <b.jeoffrey@gmail.com>
2022-01-09 21:56:47 -0800

    initialize code quiz code. pseudo-code script.js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/* 
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score 
+*/
+
+
+// questions data with correct answer
+// startquiz
+
+// load first question
+// go to next question
+
+// incorrect answer, subtract 5 second penalty
+
+// start timer
+// end timer
+// show results
+// save initials
+// save score
+
+// show score
